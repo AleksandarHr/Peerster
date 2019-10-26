923efef8aa2cdcbbce7891599223c5f0a1012b5d
Aleksandar Hrusanov <aleksandar.hrusanov@epfl.ch>
2019-10-26 23:40:59 +0200

    Adding a file pointer - Exercise 4

--- src/github.com/2_alt/Peerster/client/main.go
+++ src/github.com/2_alt/Peerster/client/main.go
@@ -10,6 +10,7 @@ func main() {
 	UIPortPtr := flag.String("UIPort", "8080", "Port for the UI client (default \"8080\")")
 	msgPtr := flag.String("msg", "", "message to be sent")
 	destPtr := flag.String("dest", "", "message to be sent")
+	fileToSharePtr := flag.String("file", "", "file to be indexed by the gossiper")
 	flag.Parse()
 	localAddressAndPort := "127.0.0.1:" + *UIPortPtr
 
